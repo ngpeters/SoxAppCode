7859167b2cd7dc87dc55be8edfb652d2ce59d946
ngpeters <27ngpeters@gmail.com>
2017-04-30 13:01:35 -0600

    launch screen changes

---
+++ PrototypeTwo/PrototypeTwo/LaunchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LaunchViewController.swift
+//  PrototypeTwo
+//
+//  Created by Natalie Peters on 4/30/17.
+//  Copyright © 2017 Natalie Peters. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 3.0)
+        return true
+    }
+}
